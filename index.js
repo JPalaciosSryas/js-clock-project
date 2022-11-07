e6786d6a8f09451d437d821ddb7c9be6aa08a4c3
JPalaciosSryas <juan.palacios@sryas.com>
2022-11-07 16:43:00 -0600

    date-screen

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let clock = document.getElementById('clock');
+let dateScreen = document.getElementById('date');
+let ordinal = document.getElementById('ordinal');
+let yearScreen = document.getElementById('year');
 
 const startTime = () => {
 
@@ -52,3 +55,28 @@ startTime();
 // };
 
 // x > 10 ? console.log('true') : console.log('false');
+
+startDate = () => {
+    var ord;
+    var today = new Date();
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var currentDay = today.getDate();
+    var currentMonth = months[today.getMonth()];
+    var currentYear = today.getFullYear();
+    if (currentDay == 1) {
+        ord = 'st'
+    } else if (currentDay == 3) {
+        ord = 'rd'
+    } else if (currentDay == 2) {
+        ord = 'nd'
+    } else  {
+        ord = 'th'
+    }
+
+    var date = currentMonth + " " + currentDay; 
+    var year = currentYear;
+    dateScreen.innerText = date;
+    ordinal.innerText = ord + ' ';
+    yearScreen.innerText = year;
+}
+startDate();
